8f1677853ab4991ac437ce9e8b171a5335e4183b
Juan <juan@example.com>
2025-12-16 11:59:15 -0300

    fixed all

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    // Configuración para optimización de imágenes usando la nueva API
+    domains: ['localhost', 'www.tekohatech.com'],
+  },
+});
